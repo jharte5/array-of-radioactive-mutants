92c843e6baf053e662d758495cb628d71d1782d8
jharte5 <jonathan.harte@codeimmersives.com>
2019-10-20 21:38:28 -0400

    changed all values

--- main.js
+++ main.js
@@ -3,17 +3,19 @@ function changeLast(arr,num) {
 
 }
 
-
 function addMeToEnd(arr) {
   arr[arr.push('Colin')]
 }
 
-function addMeToStart() {
-
+function addMeToStart(arr) {
+  arr[arr.unshift('Colin')]
 }
 
-function changeAllValuesTo() {
-
+function changeAllValuesTo(a,v) {
+let i, num = a.length;
+    for (i = 0; i < num; ++i) {
+        a[i] = v;
+    }
 }
 
 function oddOrEven() {
